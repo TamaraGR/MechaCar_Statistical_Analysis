7708b15713b6bb176ed2752c0b7fe4a1442b6720
Tamara Grigoryeva <50555637+TamaraGR@users.noreply.github.com>
2021-03-19 11:10:54 -0400

    deliverable II

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,12 @@
 # Challenge Deliverable I
 library(dplyr) # import dplyr dependency
-mechacar_challenge <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) #read the csv file as dataframe
+mechacar_challenge <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F) #read the csv file as dataframe
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_challenge) #create linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_challenge)) #determine the p-value and the r-squared value for the linear regression model
+
+# Challenge Deliverable II 
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F,header=TRUE) # read the csv file as a table
+total_summary <- suspension_coil %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI),standard_deviation=sd(PSI)) #get the mean, median, variance, and standard deviation of the suspension coil's PSI column
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarise(mean=mean(PSI), median=median(PSI), variance=var(PSI), standard_deviation=sd(PSI)) #group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column
 
